18aa63561504aef74eb0318be0bc93df5fc5fe29
AlexIsCool321 <alex.m.diehl1@gmail.com>
2025-10-07 06:53:59 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App
 	public static void main(String[] args)
 	{
 		System.out.println("_   _ _____ _     _     ___   __        _____  ____  _     ____");
-		System.out.println("| | | | ____| |   | |   / _ \"  \" \"      / / _ \"|  _ \"| |   |  _ \" ");
+		System.out.println("| | | | ____| |   | |   / _ \\  \\ \\      / / _ \\|  _ \\| |   |  _ \" ");
 		System.out.println("| |_| |  _| | |   | |  | | | |  \" \" /\" / / | | | |_) | |   | | | | ");
 		System.out.println("|  _  | |___| |___| |__| |_| |   \" V  V /| |_| |  _ <| |___| |_| | ");
 		System.out.println(" |_| |_|_____|_____|_____\"___/     \"_/\"_/  \"___/|_| \"_\"_____|____/ ");
